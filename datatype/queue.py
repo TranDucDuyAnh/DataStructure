860e21e23dc65d6320f6ce8a30d299c434379cbc
TranDucDuyAnh <tranducduyanh.2018@gmail.com>
2023-04-23 21:22:15 +0700

    v1.2.0

--- datatype/queue.py
+++ datatype/queue.py
@@ -3,13 +3,18 @@ class Queue:
         self.queue = list()
 
     # Thêm phần tử mới bằng insert()
-    def add_to_queue(self, value):  # FI (First In)
+    def add(self, value):  # FI (First In)
         if value not in self.queue:
             # Dùng insert(0, giá trị) để đưa giá trị mới vào đầu list. Đẩy giá trị thêm vào trước đi lên
             # VD: Cho list = [2, 3, 4]. insert(0, 1) sẽ cho ra list = [1, 2, 3, 4]
+            print("Đã thêm phần tử giá trị", value, "vào queue")
             self.queue.insert(0, value)
         else:
-            print("Gia tri da nam trong queue")
+            print("Giá trị đã nằm trong queue!")
+
+    def print_queue(self):
+        print("Queue hiện tại:", self.queue)
+        print("Kích thước của queue:", self.size())
 
     # Kích thước của queue
     def size(self):
@@ -19,19 +24,35 @@ class Queue:
     # Hàm pop() bỏ phần tử ở cuối list
     def remove(self):  # FO (First Out)
         if len(self.queue) > 0:
+            print("Phần tử", self.queue[-1], "đã bị xóa khỏi queue")
             self.queue.pop()
         else:
             return "Queue không có phần tử nào"
 
 
-test_queue = Queue()
-test_queue.add_to_queue("Mon")
-test_queue.add_to_queue("Tue")
-test_queue.add_to_queue("Wed")
-print(test_queue.queue)
-print(test_queue.size())
-
-test_queue.remove()
-print(test_queue.queue)
-print(test_queue.size())
+if __name__ == "__main__":
+    test_queue = Queue()
+    first_val = input("Nhập một giá trị để bắt đầu queue: ")
+    test_queue.add(first_val)
+    finish = False
+    while finish is not True:
+        print()
+        print("HELP: 1 - Thêm, 2 - In, 3 - Xóa, 4 - Kết thúc")
+        select = abs(int(input("Chọn 1 lệnh để thực hiện: ")))
+        if (select > 4) or (select < 1):
+            print("Số không hợp lệ!")
+            continue
+        elif select == 1:
+            next_val = input("Nhập một giá trị bất kì để thêm vào queue: ")
+            test_queue.add(next_val)
+            continue
+        elif select == 2:
+            test_queue.print_queue()
+            continue
+        elif select == 3:
+            test_queue.remove()
+            continue
+        elif select == 4:
+            finish = True
+            print("Chương trình kết thúc...")
 
--- datatype/stack.py
+++ datatype/stack.py
@@ -7,9 +7,14 @@ class Stack:
     def add(self, value):
         # Thêm phần tử vào cuối list dùng append() (LI - Last In)
         if value not in self.stack:
+            print("Đã thêm phần tử giá trị", value)
             self.stack.append(value)
         else:
-            print("Gia tri da nam trong stack!")
+            print("Giá trị đã nằm trong stack!")
+
+    def print_stack(self):
+        print("Stack hiện tại:", self.stack)
+        print("Giá trị nằm trên cùng stack:", self.peek())
 
     # Hàm peek() để trả giá trị được thêm cuối cùng vào stack
     def peek(self):
@@ -20,28 +25,33 @@ class Stack:
         if len(self.stack) <= 0:
             return "Không thể bỏ giá trị nào"
         else:
-            return self.stack.pop()
+            print("Đã xóa phần tử giá trị", self.peek())
+            self.stack.pop()
 
 
-# Tạo stack
-test_stack = Stack()
+if __name__ == "__main__":
+    test_stack = Stack()
+    first_val = input("Nhập một giá trị để bắt đầu stack: ")
+    test_stack.add(first_val)
+    finish = False
+    while finish is not True:
+        print()
+        print("HELP: 1 - Thêm, 2 - In, 3 - Xóa, 4 - Kết thúc")
+        select = abs(int(input("Chọn 1 lệnh để thực hiện: ")))
+        if (select > 4) or (select < 1):
+            print("Số không hợp lệ!")
+            continue
+        elif select == 1:
+            next_val = input("Nhập một giá trị bất kì để thêm vào stack: ")
+            test_stack.add(next_val)
+            continue
+        elif select == 2:
+            test_stack.print_stack()
+            continue
+        elif select == 3:
+            test_stack.remove()
+            continue
+        elif select == 4:
+            finish = True
+            print("Chương trình kết thúc...")
 
-# Thêm 2 phần tử vào stack
-test_stack.add("Mon")
-test_stack.add("Tue")
-
-# Xem phần tử thêm vào cuối cùng. Trả về "Tue"
-print(test_stack.peek())
-
-# Thêm 2 phần tử vào stack
-test_stack.add("wed")
-test_stack.add("Thu")
-
-# Xem phần tử thêm vào cuối cùng. Trả về "Thu"
-print(test_stack.peek())
-
-# Bỏ phần tử thêm vào cuối cùng
-test_stack.remove()
-
-# Xem phần tử thêm vào cuối cùng. Trả về "Wed"
-print(test_stack.peek())
