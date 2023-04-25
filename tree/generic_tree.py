ed9652b63b6f2cc639dcdbc86ea6563b3aa94306
TranDucDuyAnh <tranducduyanh.2018@gmail.com>
2023-04-25 19:48:59 +0700

    v1.2.0

---
+++ tree/generic_tree.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, value):
+        self.children = []
+        self.value = value
+
+    def add_child(self, child):
+        print("Đã thêm nút con", child.value, "vào nút", self.value)
+        self.children.append(child)
+
+
+class GenericTree:
+    def __init__(self):
+        self.root = None
+
+    def add_root_node(self, value):
+        print("Đã thêm nút giá trị", value, "là nút gốc của cây")
+        self.root = Node(value)
+
+    def find_node_to_add(self, curr, child, father):
+        if curr.value == father:
+            curr.add_child(Node(child))
+            return
+        if len(curr.children) != 0:
+            for i in range(0, len(curr.children)):
+                self.find_node_to_add(curr.children[i], child, father)
+
+    def print_all_nodes(self, curr):
+        print(curr.value, "->", [child.value for child in curr.children])
+        if len(curr.children) != 0:  # Kiểm tra nếu nút có con, nếu có thì duyệt các nút con từ trái -> phải
+            for i in range(0, len(curr.children)):
+                self.print_all_nodes(curr.children[i])
+
+
+# Duyệt theo thứ tự trước dùng đệ quy: Node -> Left -> Right, bắt đầu từ nút gốc
+def preorder_traversal(node):
+    print(node.value, end=" ")  # In giá trị của nút ra
+    if len(node.children) != 0:  # Kiểm tra nếu nút có con, nếu có thì duyệt các nút con từ trái -> phải
+        for i in range(0, len(node.children)):
+            preorder_traversal(node.children[i])
+
+
+# Duyệt theo thứ tự sau dùng đệ quy: Left -> Right -> Node, bắt đầu từ nút gốc
+def postorder_traversal(node):
+    if len(node.children) != 0:  # Kiểm tra nếu nút có con, nếu có thì duyệt các nút con từ trái -> phải
+        for i in range(0, len(node.children)):
+            postorder_traversal(node.children[i])
+    print(node.value, end=" ")  # In giá trị của nút ra
+
+
+if __name__ == '__main__':
+    first_val = input("Nhập một giá trị để bắt đầu một cây cơ bản: ")
+    generic_tree = GenericTree()
+    generic_tree.add_root_node(first_val)
+    finish = False
+    while finish is not True:
+        print()
+        print("HELP: 1 - Danh sách nút, 2 - Thêm nút con, 3 - Duyệt cây, 4 - Kết thúc")
+        select = abs(int(input("Chọn 1 lệnh để thực hiện: ")))
+        if select == 1:
+            generic_tree.print_all_nodes(generic_tree.root)
+            continue
+        elif select == 2:
+            child_node = input("Nhập giá trị của nút con mới: ")
+            parent_node = input("Nhập giá trị của nút cha: ")
+            generic_tree.find_node_to_add(generic_tree.root, child_node, parent_node)
+            continue
+        elif select == 3:
+            choice = abs(int(input("Chọn cách duyệt: 1 - Trước, 2 - Sau: ")))
+            if choice == 1:
+                preorder_traversal(generic_tree.root)
+            elif choice == 2:
+                postorder_traversal(generic_tree.root)
+            else:
+                print("Số không hợp lệ!")
+            continue
+        elif select == 4:
+            finish = True
+            print("Chương trình kết thúc...")
+        else:
+            print("Số không hợp lệ!")
